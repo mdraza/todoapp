faa916d62fe18f159196ed83980d86c95f403b06
mdraza <raza.uidev@gmail.com>
2020-08-16 02:07:34 +0530

    initial commit

---
+++ precache-manifest.7ae5ffbd89ff1023d7c81f3cb19cfd57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2244ce03f7f4f6b69646533721dfeb84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "658d22f07a8879a28999",
+    "url": "/static/css/main.1351b15f.chunk.css"
+  },
+  {
+    "revision": "a31f54d2b25f91f749cc",
+    "url": "/static/js/2.b606d4e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b606d4e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "658d22f07a8879a28999",
+    "url": "/static/js/main.2e12419b.chunk.js"
+  },
+  {
+    "revision": "bf250259d9fd61dc7dbe",
+    "url": "/static/js/runtime-main.ecec8c93.js"
+  }
+]);
